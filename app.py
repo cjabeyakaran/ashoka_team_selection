c383a20542aa48c9f011f0879662a67ed096976e
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-09-11 16:55:13 -0500

    flask file and template created

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
 from flask import Flask, render_template, request
 from program import Program
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+program = Program()
 
 # render index.html
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 # upload team prefs
 @app.route('/teamprefs', methods=['POST'])
 def team_upload():
     if request.method == 'POST':
-        file = request.files['file']
-        # file.save(file.filename)
-        print(file.read())
+        f = request.files['file']
+        filename = secure_filename()
+        f.save(filename)
+        program.handle_teams_file(filename)
+        return "True"
+        
 
 # upload student prefs
 @app.route('/studentprefs', methods=['POST'])
 def student_upload():
     if request.method == 'POST':
-        file = request.files['file']
-        # file.save(file.filename)
-        print(file.read())
+        f = request.files['file']
+        filename = secure_filename()
+        f.save(filename)
+        program.handle_students_file(filename)
+        return "True"
 
 # run matching algo
 # @app.route('/match')
