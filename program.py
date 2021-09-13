c63382df2b6adcc3bed8168e9fb5a4b2e14305bf
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-09-12 22:46:30 -0500

    barebones handle teams file

--- program.py
+++ program.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from classes.algo import Algo
 
-# check later for column names, especially gender
+# check later for column names
+# mainly gender in students
+# and boy prefs and girl prefs in dancers
 
 class Program:
     def __init__(self):
@@ -28,3 +30,29 @@ class Program:
         # import spreadsheet and create dancer object for each row
         df = pd.read_excel(file_path)
         df.apply(create_dancer, axis=1)
+
+    def handle_teams_file(self, file_path):
+        # helper function to convert row into team girls and boys objects
+        df = pd.read_excel(file_path, index_col="Team:")
+
+        team_names = ["Raas", "Garba", "Chaahat", "Bhangra"]
+        for name in team_names:
+            girl_prefs = df.loc[name, "List girl dancers/singers in order of preference:"]
+            max_girls = df.loc[name, "max girls"]
+            
+            self.algo.add_team(f"{name} - Girls", girl_prefs, max_girls)
+
+            if name != "Garba":
+                boy_prefs = df.loc[name, "List boy dancers/singers in order of preference:"]
+                max_boys = df.loc[name, "max boys"]
+            
+                self.algo.add_team(f"{name} - Boys", boy_prefs, max_boys)
+
+
+    def run_matching(self):
+        return self.algo.run()
+
+            
+
+
+        
