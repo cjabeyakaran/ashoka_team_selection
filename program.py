c4d1383b74488d649c36a6fe5897794f2e3c2145
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-09-13 21:57:20 -0500

    Finalized columns

--- program.py
+++ program.py
@@ -1,10 +1,6 @@
 import pandas as pd
 from classes.algo import Algo
 
-# check later for column names
-# mainly gender in students
-# and boy prefs and girl prefs in dancers
-
 class Program:
     def __init__(self):
         self.algo = Algo()
@@ -13,15 +9,15 @@ class Program:
         # helper function to convert row into dancer object
         def create_dancer(row):
             prefs = []
-            gender = row["Gender"] # mark dancer as boy vs girl
+            gender = row["Gender"] # mark dancer as Male vs Female
 
             cols = ["Preference 1:", "Preference 2:", "Preference 3:", "Preference 4:", "Preference 5:"]
             for col in cols:
                 if pd.notnull(row[col]):
                     prefs.append(f"{row[col]} - {gender}s")
 
-                    if row[col] == "Raas" and gender == "Girls":
-                        prefs.append(f"{row[col]} - Boys")
+                    if row[col] == "Raas" and gender == "Female":
+                        prefs.append(f"{row[col]} - Males")
 
             self.algo.add_dancer(row["Name:"], prefs)
 
@@ -32,25 +28,25 @@ class Program:
         df.apply(create_dancer, axis=1)
 
     def handle_teams_file(self, file_path):
-        # helper function to convert row into team girls and boys objects
+        # helper function to convert row into team Females and team Males objects
         df = pd.read_excel(file_path, index_col="Team:")
 
         team_names = ["Raas", "Garba", "Chaahat", "Bhangra"]
         for name in team_names:
             girl_prefs = df.loc[name, "List girl dancers/singers in order of preference:"]
-            max_girls = df.loc[name, "max girls"]
+            max_girls = df.loc[name, "Ideally, how many girls are you looking to recruit this year?"]
             
-            self.algo.add_team(f"{name} - Girls", girl_prefs, max_girls)
+            self.algo.add_team(f"{name} - Females", girl_prefs, max_girls)
 
             if name != "Garba":
                 boy_prefs = df.loc[name, "List boy dancers/singers in order of preference:"]
-                max_boys = df.loc[name, "max boys"]
+                max_boys = df.loc[name, "Ideally, how many boys are you looking to recruit this year?"]
             
-                self.algo.add_team(f"{name} - Boys", boy_prefs, max_boys)
+                self.algo.add_team(f"{name} - Males", boy_prefs, max_boys)
 
 
     def run_matching(self):
-        return self.algo.run()
+        self.algo.run()
 
             
 
