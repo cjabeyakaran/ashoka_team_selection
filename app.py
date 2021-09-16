dc76082031951c02a83d7c020c98f5e2724f101e
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-09-15 22:52:19 -0500

    Fixed layout of rosters

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 from program import Program
 from werkzeug.utils import redirect, secure_filename
+import os
+
+curr = os.getcwd()
+UPLOAD_FOLDER = os.path.join(curr, "uploads")
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 program = Program()
 
@@ -11,31 +16,21 @@ program = Program()
 def index():
     return render_template('index.html')
 
-# upload team prefs
-@app.route('/teamprefs', methods=['POST'])
-def team_upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        filename = secure_filename(f.filename)
-        f.save(filename)
-        program.handle_teams_file(filename)
-        return redirect(request.referrer)
-        
-
-# upload student prefs
-@app.route('/studentprefs', methods=['POST'])
-def student_upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        filename = secure_filename(f.filename)
-        f.save(filename)
-        program.handle_students_file(filename)
-        return redirect(request.referrer)
-
 # run matching algo
-@app.route('/match')
+@app.route('/match', methods = ['POST'])
 def match():
-    return program.run_matching()
+    # return program.run_matching()
+    team_f = request.files['team-file']
+    team_filepath = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(team_f.filename))
+    team_f.save(team_filepath)
+    program.handle_teams_file(team_filepath)
+
+    dancer_f = request.files['dancer-file']
+    dancer_filepath = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(dancer_f.filename))
+    dancer_f.save(dancer_filepath)
+    program.handle_students_file(dancer_filepath)
+
+    return render_template('rosters.html', rosters=program.run_matching())
 
 if __name__ == '__main__':
     app.run()
